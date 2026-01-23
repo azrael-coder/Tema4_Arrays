00bb37e8c8ded62945015e408e8d900616d35cf0
Chris <tu@email.com>
2026-01-23 10:15:12 +0100

    arraysss

---
+++ src/main/java/pruebas/Array1.java
@@ -0,0 +1,14 @@
+package pruebas;
+
+public class Array1 {
+    public static void main(String[] args) {
+        int [] array = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
+        double [] victor = {2.3, 4.7, 1.9};
+
+
+
+
+
+    }
+}
