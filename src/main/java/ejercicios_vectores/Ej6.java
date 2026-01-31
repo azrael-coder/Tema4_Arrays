46c779b7598f13fb146e631f95a4de5ae234c069
azrael-coder <israelgonzalez050908@gmail.com>
2026-01-31 17:45:03 +0100

    arreglao

--- src/main/java/ejercicios_vectores/Ej6.java
+++ src/main/java/ejercicios_vectores/Ej6.java
@@ -7,13 +7,13 @@ package ejercicios_vectores;
  */
 public class Ej6 {
     public static void main(String[] args) {
-        int[] vector = {1,2,3,4,2,3};
+        int[] vector = {1,2,3,4,2,3,2,4};
 
         int cantidad = aparicionesNum(vector);
         System.out.println(cantidad);
     }
 
-    public static int contarApariciones(int num, int[] vector){
+    private static int contarApariciones(int num, int[] vector){
         int contador = 0;
 
         for(int i=0; i<vector.length; i++){
@@ -24,22 +24,27 @@ public class Ej6 {
         return contador;
     }
 
-    public static int aparicionesNum(int[] vector){
-        int numRepetidos = 0;
-        int numActual;
-        int apariciones;
+    public static int aparicionesNum(int[] vector){//ARREGLAO
+        int cantidadReps = 0;
+        boolean aparecido = false;
 
-            for(int i=0; i<vector.length; i++){ //CORREGIR !!!!
-                numActual = vector[i];
+            for(int i=0; i<vector.length; i++){
 
-                if (contarApariciones(vector[i], vector) == 0) {
-                    apariciones = contarApariciones(vector[i], vector);
+                for(int j=0; j < i; j++){
+                    if(vector[i] == vector[j]){
+                        aparecido = true;
+                    }
                 }
-                if (contarApariciones(vector[i], vector) >= 2) {
-                    numRepetidos++;
+
+                if (!aparecido) {
+                    int cantidad = contarApariciones(vector[i], vector);
+
+                    if(cantidad >=2){
+                        cantidadReps++;
+                    }
                 }
             }
 
-        return numRepetidos;
+        return cantidadReps;
     }
 }
