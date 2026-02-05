62977b0025b04d604c2362ff32cd8f25e0ae32b3
azrael-coder <israelgonzalez050908@gmail.com>
2026-02-05 23:03:08 +0100

    terminar 12

--- src/main/java/matrices/Ej1.java
+++ src/main/java/matrices/Ej1.java
@@ -4,16 +4,18 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class Ej1 {
     public static void main(String[] args) {
-        int[][] matriz = new int[5][4];
-
+        int[][] matriz = new int[4][4];
         llenarTabla(matriz);
 
         //int suma = sumaTotal(matriz);
         //double media = calcularMedia(matriz);
         //tring buscarNum = buscarNum(4, matriz);
-        int[][] invert=transponer(matriz);
-
-        System.out.println(Arrays.deepToString(invert));
+        //int[][] invert=transponer(matriz);
+        //int[] columnaExtraida = extrearColumna(4,matriz);
+        //int[] filaExtraida = extraerFila(1, matriz);
+        //int[][] copia = duplicarMatriz(matriz);
+        int[][] subMatriz = crearSubtabla(matriz,1,1,3,3);
+        System.out.println(Arrays.deepToString(subMatriz));
     }
 
     /**
@@ -28,7 +30,6 @@ public class Ej1 {
         System.out.println("Ingrese el numero final: ");
         int numFinal = sc.nextInt();
 
-
         if (matriz != null) {
             for (int fil = 0; fil < matriz.length; fil++) {
                 for (int col = 0; col < matriz[fil].length; col++) {
@@ -41,7 +42,7 @@ public class Ej1 {
     }
 
     /**
-     * EJ2:     Calcula y devuelve la suma de todos los elementos de la tabla
+     * EJ2: Calcula y devuelve la suma de todos los elementos de la tabla
      * @param matriz
      * @return suma
      */
@@ -130,7 +131,89 @@ public class Ej1 {
                     invert[j][i] = matriz[i][j];
                 }
             }
-
         return invert;
+    }
+
+    /**
+     * EJ10: Recibe el índice de una columna y devuelve un arreglo
+     * unidimensional con los elementos de esa columna. Si la columna no existe,
+     * muestra un mensaje de error
+     */
+    public static int [] extrearColumna(int columna, int[][] matriz){
+        int[] col = new int[matriz.length];
+        if (columna > 0 && columna < matriz[0].length && matriz != null) {
+
+            for (int i = 0; i < matriz.length; i++) {
+                col[i]= matriz[i][columna];
+            }
+        }else{
+            System.out.println("ERROR: Columna no encontrada");
+        }
+        return col;
+    }
+
+    /**
+     * EJ11:Recibe el índice de una fila y devuelve un arreglo unidimensional
+     * con los elementos de esa fila. Si la fila no existe, muestra un mensaje de error
+     * @param fila
+     * @param matriz
+     * @return fil
+     */
+    public static int[] extraerFila(int fila, int[][] matriz){
+        int[] fil = new int[matriz[0].length];
+
+        if (fila > 0 && fila < matriz.length && matriz != null) {
+            for (int j = 0; j < matriz[0].length; j++ ) {
+                fil[j] = matriz[fila][j];
+            }
+        }else{
+            System.out.println("ERROR: Fila no encontrada");
+        }
+        return fil;
+    }
+
+    /**
+     * Crea y devuelve una nueva tabla que es una copia exacta de la
+     * tabla original
+     * @param matriz
+     * @return
+     */
+    public static int[][] duplicarMatriz(int[][] matriz){
+        int[][] copia = new int[matriz.length][matriz[0].length];
+            if (matriz != null) {
+                for (int i= 0; i < matriz.length; i++) {
+                    for (int j = 0; j < matriz[i].length; j++){
+                        copia[i][j] = matriz[i][j];
+                    }
+                }
+            }
+        return copia;
+    }
+
+
+
+    /**
+     * EJ13: Recibe las posiciones de inicio de fila y columna y el tamaño de la
+     * subtabla que se desea extraer. Devuelve una nueva tabla con los elementos de la
+     * subtabla indicada.
+     * @param matriz
+     * @param filInicio
+     * @param colInicio
+     * @param filas
+     * @param columnas
+     * @return submatriz
+     */
+    public static int[][] crearSubtabla(int[][] matriz, int filInicio, int colInicio, int filas, int columnas){
+        int[][] submatriz = new int[filas][columnas];
+        int subIndiceFil = 0;
+        int subIndiceCol = 0;
+        for (int i = filInicio; i < filas; i++){
+            for (int j = colInicio; j < columnas; j++){
+                submatriz[subIndiceFil][subIndiceCol] = matriz[i][j];
+                subIndiceCol++;
+            }
+            subIndiceFil++;
+        }
+        return submatriz;
     }
 }
